d3cb8adb974af5034377c8eab2ffe8c71a7f559f
Mamniash <mamniashvili2003.com>
2025-08-04 11:47:45 +0300

    Добавил Яндекс метрики

--- src/app/YandexMetrika.tsx
+++ src/app/YandexMetrika.tsx
@@ -9,17 +9,23 @@ export const YandexMetrika = () => (
 			strategy='afterInteractive'
 			dangerouslySetInnerHTML={{
 				__html: `
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+              if (document.scripts[j].src === r) { return; }
+            }
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+            k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=103585501", "ym");
 
-          ym(102073661, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
+          ym(103585501, "init", {
+            ssr: true,
+            webvisor: true,
+            clickmap: true,
+            ecommerce: "dataLayer",
+            accurateTrackBounce: true,
+            trackLinks: true
           });
         `
 			}}
@@ -27,7 +33,7 @@ export const YandexMetrika = () => (
 		<noscript>
 			<div>
 				<img
-					src='https://mc.yandex.ru/watch/102073661'
+					src='https://mc.yandex.ru/watch/103585501'
 					style={{ position: 'absolute', left: '-9999px' }}
 					alt=''
 				/>
